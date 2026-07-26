08259d91d54eee3a98ccbebd0b9aecf60f812cde
laabibi <admirablea42@gmail.com>
2026-07-26 17:51:32 +0100

    Add RoleMiddleware for role-based access control

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,36 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'main')->name('home');
-Route::view('/menu', 'menu')->name('menu');
-Route::view('/service', 'service')->name('service');
-Route::view('/about', 'about-us')->name('about');
-Route::view('/contact', 'contact-us')->name('contact');
-Route::view('/team', 'our-team')->name('team');
-Route::view('/testimonial', 'testimonial')->name('testimonial');
-Route::view('/cart', 'cart')->name('cart');
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginAuth'])->name('login.submit');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-Route::post('/register', [AuthController::class, 'registerAuth'])->name('register.submit');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth', 'role:client'])->group(function () {
+    // Client routes
+    Route::view('/', 'main')->name('home');
+    Route::view('/menu', 'menu')->name('menu');
+    Route::view('/service', 'service')->name('service');
+    Route::view('/about', 'about-us')->name('about');
+    Route::view('/contact', 'contact-us')->name('contact');
+    Route::view('/team', 'our-team')->name('team');
+    Route::view('/testimonial', 'testimonial')->name('testimonial');
+    Route::view('/cart', 'cart')->name('cart');
+});
+
+
+Route::middleware(['auth', 'role:admin,kitchen_worker,delivery_worker'])
+    ->prefix('dashboard')
+    ->group(function () {
+
+        Route::get('/', function () {
+            return 'Dashboard';
+        })->name('admin.dashboard');
+    });
+
+Route::middleware(['guest'])->group(function () {
+    // Guest routes
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginAuth'])->name('login.submit');
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/register', [AuthController::class, 'registerAuth'])->name('register.submit');
+});
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
